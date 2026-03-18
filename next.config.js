e62d5a105461c6355547d99483dbabd2ac69c1fe
fenerlicon <aslanbasardafurkan@gmail.com>
2026-03-19 01:13:52 +0300

    chore(tiktok): add new verification file and rewrite rules

--- next.config.js
+++ next.config.js
@@ -14,6 +14,10 @@ const nextConfig = {
         source: '/tiktokbqod9Dsb8AiJnWB3R7037aLZA191TBEP.txt/',
         destination: '/tiktokbqod9Dsb8AiJnWB3R7037aLZA191TBEP.txt',
       },
+      {
+        source: '/tiktokennC3hH91oikg0dKY5dyXqqVMLRuJPzc.txt/',
+        destination: '/tiktokennC3hH91oikg0dKY5dyXqqVMLRuJPzc.txt',
+      },
     ]
   },
 }
